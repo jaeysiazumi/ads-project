270d9682ad660324d8d963b1fd9ede8437da7046
Lorraine <Lorraine@LAPTOP-387D4Q4D>
2026-03-23 21:21:43 +0800

    dbconnect

---
+++ ADSFinalProject/src/adsfinalproject/DBConnection.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adsfinalproject;
+
+/**
+ *
+ * @author Lorraine
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/foodorderingdb",
+                "root",
+                ""
+            );
+            System.out.println("Connected!");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
+    
+}
